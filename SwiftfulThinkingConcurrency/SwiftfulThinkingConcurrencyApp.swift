258e875602ae088b95bf1b8520b5927749062303
Pavlo Bezkorovainyi <86742948+pavlo-bezkorovainyi@users.noreply.github.com>
2024-03-25 21:32:10 +0200

    Add files via upload

---
+++ SwiftfulThinkingConcurrency/SwiftfulThinkingConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingConcurrencyApp.swift
+//  SwiftfulThinkingConcurrency
+//
+//  Created by Павел Бескоровайный on 25.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
